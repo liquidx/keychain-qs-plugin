b384daf0d268e0684f631958894cfaf7c7ff5d53
acnt2 <acnt2@d2701e31-9253-0410-a10d-edde030dbc2a>
2005-04-17 18:57:36 +0000

    initial import of Keychain Plugin for QuickSilver

---
+++ ATKeychainPlugin/trunk/Keychain_Module_Action.m
@@ -0,0 +1,19 @@
+//
+//  Keychain_Module_Action.m
+//  Keychain Module
+//
+//  Created by Alastair on 27/03/2005.
+//  Copyright __MyCompanyName__ 2005. All rights reserved.
+//
+
+#import "Keychain_Module_Action.h"
+
+@implementation Keychain_Module_Action
+
+
+#define kKeychain_ModuleAction @"Keychain_ModuleAction"
+
+- (QSObject *)performActionOnObject:(QSObject *)dObject{
+	return nil;
+}
+@end
---
+++ ATKeychainPlugin/trunk/Keychain_Module_Source.h
@@ -0,0 +1,16 @@
+//
+//  Keychain_Module_Source.h
+//  Keychain Module
+//
+//  Created by Alastair on 27/03/2005.
+//  Copyright __MyCompanyName__ 2005. All rights reserved.
+//
+
+
+#import "Keychain_Module_Source.h"
+
+@interface Keychain_Module_Source : NSObject
+{
+}
+@end
+
